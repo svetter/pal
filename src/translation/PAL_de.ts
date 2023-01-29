e2a9317f19abde7f769200fd9b2d1e8366eb267d
Simon Vetter <simon_michael.vetter@stud.tu-darmstadt.de>
2023-01-29 18:32:55 +0100

    Updated German translation

--- src/translation/PAL_de.ts
+++ src/translation/PAL_de.ts
@@ -2,139 +2,353 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="de_DE">
 <context>
-    <name>AddAscentDialog</name>
-    <message>
-        <location filename="src/ui/addascent.ui" line="14"/>
-        <source>Dialog</source>
-        <translation type="unfinished"></translation>
-    </message>
-</context>
-<context>
     <name>MainWindow</name>
     <message>
-        <location filename="src/ui/mainwindow.ui" line="76"/>
         <source>Add ascent...</source>
-        <translation>Neue Besteigung...</translation>
+        <translation type="vanished">Neue Besteigung...</translation>
     </message>
     <message>
-        <location filename="src/ui/mainwindow.ui" line="110"/>
         <source>Add peak...</source>
-        <translation>Neuer Gipfel...</translation>
+        <translation type="vanished">Neuer Gipfel...</translation>
     </message>
     <message>
-        <location filename="src/ui/mainwindow.ui" line="129"/>
         <source>Add climber...</source>
-        <translation>Neuer Wanderer...</translation>
+        <translation type="vanished">Neuer Wanderer...</translation>
     </message>
     <message>
-        <location filename="src/ui/mainwindow.ui" line="148"/>
         <source>Add trip...</source>
-        <translation>Neue Reise...</translation>
+        <translation type="vanished">Neue Reise...</translation>
     </message>
     <message>
-        <location filename="src/ui/mainwindow.ui" line="183"/>
+        <location filename="../ui/main_window.ui" line="182"/>
         <source>Logged ascents:</source>
         <translation>Eingetragene Besteigungen:</translation>
     </message>
     <message>
-        <location filename="src/ui/mainwindow.ui" line="239"/>
+        <location filename="../ui/main_window.ui" line="238"/>
         <source>Ascents</source>
         <translation>Besteigungen</translation>
     </message>
     <message>
-        <location filename="src/ui/mainwindow.ui" line="247"/>
+        <location filename="../ui/main_window.ui" line="246"/>
         <source>Peaks</source>
         <translation>Gifpel</translation>
     </message>
     <message>
-        <location filename="src/ui/mainwindow.ui" line="252"/>
+        <location filename="../ui/main_window.ui" line="261"/>
         <source>Climbers</source>
         <translation>Wanderer</translation>
     </message>
     <message>
-        <location filename="src/ui/mainwindow.ui" line="257"/>
+        <location filename="../ui/main_window.ui" line="256"/>
         <source>Trips</source>
         <translation>Reisen</translation>
     </message>
     <message>
-        <location filename="src/ui/mainwindow.ui" line="277"/>
+        <location filename="../ui/main_window.ui" line="251"/>
+        <source>Mountain ranges</source>
+        <translation>Gebirge</translation>
+    </message>
+    <message>
+        <location filename="../ui/main_window.ui" line="281"/>
         <source>File</source>
         <translation>Datei</translation>
     </message>
     <message>
-        <location filename="src/ui/mainwindow.ui" line="290"/>
+        <location filename="../ui/main_window.ui" line="294"/>
         <source>Help</source>
         <translation>Hilfe</translation>
     </message>
     <message>
-        <location filename="src/ui/mainwindow.ui" line="296"/>
+        <location filename="../ui/main_window.ui" line="300"/>
         <source>View</source>
         <translation>Ansicht</translation>
     </message>
     <message>
-        <location filename="src/ui/mainwindow.ui" line="301"/>
+        <location filename="../ui/main_window.ui" line="305"/>
         <source>New</source>
         <translation>Neu</translation>
     </message>
     <message>
-        <location filename="src/ui/mainwindow.ui" line="320"/>
+        <location filename="../ui/main_window.ui" line="325"/>
         <source>New database...</source>
         <translation>Neue Datenbank...</translation>
     </message>
     <message>
-        <location filename="src/ui/mainwindow.ui" line="325"/>
+        <location filename="../ui/main_window.ui" line="330"/>
         <source>Open database...</source>
         <translation>Datenbank öffnen...</translation>
     </message>
     <message>
-        <location filename="src/ui/mainwindow.ui" line="333"/>
+        <location filename="../ui/main_window.ui" line="339"/>
         <source>Save database</source>
         <translation>Datenbank speichern</translation>
     </message>
     <message>
-        <location filename="src/ui/mainwindow.ui" line="341"/>
+        <location filename="../ui/main_window.ui" line="347"/>
         <source>Close database</source>
         <translation>Datenbank schließen</translation>
     </message>
     <message>
-        <location filename="src/ui/mainwindow.ui" line="346"/>
+        <location filename="../ui/main_window.ui" line="352"/>
         <source>About...</source>
         <translation>Über PAL...</translation>
     </message>
     <message>
-        <location filename="src/ui/mainwindow.ui" line="351"/>
+        <location filename="../ui/main_window.ui" line="357"/>
         <source>Open recent database</source>
         <translation>Zuletzt geöffnete Datenbanken</translation>
     </message>
     <message>
-        <location filename="src/ui/mainwindow.ui" line="356"/>
+        <location filename="../ui/main_window.ui" line="362"/>
         <source>Settings...</source>
         <translation>Einstellungen...</translation>
     </message>
     <message>
-        <location filename="src/ui/mainwindow.ui" line="361"/>
+        <location filename="../ui/main_window.ui" line="94"/>
+        <location filename="../ui/main_window.ui" line="367"/>
         <source>New ascent...</source>
         <translation>Neue Besteigung...</translation>
     </message>
     <message>
-        <location filename="src/ui/mainwindow.ui" line="366"/>
+        <location filename="../ui/main_window.ui" line="128"/>
+        <location filename="../ui/main_window.ui" line="372"/>
         <source>New peak...</source>
         <translation>Neuer Gipfel...</translation>
     </message>
     <message>
-        <location filename="src/ui/mainwindow.ui" line="371"/>
+        <location filename="../ui/main_window.ui" line="377"/>
+        <source>New mountain range...</source>
+        <translation>Neues Gebirge...</translation>
+    </message>
+    <message>
+        <location filename="../ui/main_window.ui" line="399"/>
         <source>New climber...</source>
         <translation>Neuer Wanderer...</translation>
     </message>
     <message>
-        <location filename="src/ui/mainwindow.ui" line="376"/>
+        <location filename="../ui/main_window.ui" line="147"/>
+        <location filename="../ui/main_window.ui" line="382"/>
         <source>New trip...</source>
         <translation>Neue Reise...</translation>
     </message>
     <message>
-        <location filename="src/ui/mainwindow.ui" line="384"/>
+        <location filename="../ui/main_window.ui" line="391"/>
         <source>Save database as...</source>
         <translation>Datenbank speichern unter...</translation>
+    </message>
+</context>
+<context>
+    <name>NewAscentDialog</name>
+    <message>
+        <location filename="../ui/new_ascent.ui" line="20"/>
+        <source>New ascent</source>
+        <translation>Neue Besteigung</translation>
+    </message>
+    <message>
+        <location filename="../ui/new_ascent.ui" line="49"/>
+        <source>Peak</source>
+        <translation>Gipfel</translation>
+    </message>
+    <message>
+        <location filename="../ui/new_ascent.ui" line="67"/>
+        <source>Select</source>
+        <translation>Auswählen</translation>
+    </message>
+    <message>
+        <location filename="../ui/new_ascent.ui" line="74"/>
+        <source>New peak...</source>
+        <translation>Neuer Gipfel...</translation>
+    </message>
+    <message>
+        <location filename="../ui/new_ascent.ui" line="89"/>
+        <source>Date</source>
+        <translation>Datum</translation>
+    </message>
+    <message>
+        <location filename="../ui/new_ascent.ui" line="112"/>
+        <source>Local time</source>
+        <translation>Lokalzeit</translation>
+    </message>
+    <message>
+        <location filename="../ui/new_ascent.ui" line="119"/>
+        <source>During trip</source>
+        <translation>Während Reise</translation>
+    </message>
+    <message>
+        <location filename="../ui/new_ascent.ui" line="131"/>
+        <location filename="../ui/new_ascent.ui" line="184"/>
+        <source>None</source>
+        <translation>Keine</translation>
+    </message>
+    <message>
+        <location filename="../ui/new_ascent.ui" line="144"/>
+        <source>New trip...</source>
+        <translation>Neue Reise...</translation>
+    </message>
+    <message>
+        <location filename="../ui/new_ascent.ui" line="153"/>
+        <source>Climbers</source>
+        <translation>Dabei</translation>
+    </message>
+    <message>
+        <location filename="../ui/new_ascent.ui" line="235"/>
+        <source>Notes</source>
+        <translation>Notizen</translation>
+    </message>
+    <message>
+        <location filename="../ui/new_ascent.ui" line="295"/>
+        <source>Unspecified</source>
+        <translation>Nicht angeben</translation>
+    </message>
+    <message>
+        <location filename="../ui/new_ascent.ui" line="333"/>
+        <source>Add ascent</source>
+        <translation>Besteigung hinzufügen</translation>
+    </message>
+    <message>
+        <location filename="../ui/new_ascent.ui" line="352"/>
+        <source>Cancel</source>
+        <translation>Abbrechen</translation>
+    </message>
+</context>
+<context>
+    <name>NewPeakDialog</name>
+    <message>
+        <location filename="../ui/new_peak.ui" line="20"/>
+        <source>New peak</source>
+        <translation>Neuer Gipfel</translation>
+    </message>
+    <message>
+        <location filename="../ui/new_peak.ui" line="49"/>
+        <source>Name</source>
+        <translation>Name</translation>
+    </message>
+    <message>
+        <location filename="../ui/new_peak.ui" line="56"/>
+        <source>Mountain range</source>
+        <translation>Gebirge</translation>
+    </message>
+    <message>
+        <location filename="../ui/new_peak.ui" line="68"/>
+        <source>None</source>
+        <translation>Keines</translation>
+    </message>
+    <message>
+        <location filename="../ui/new_peak.ui" line="81"/>
+        <source>New range...</source>
+        <translation>Neues Gebirge...</translation>
+    </message>
+    <message>
+        <location filename="../ui/new_peak.ui" line="90"/>
+        <source>Height</source>
+        <translation>Höhe</translation>
+    </message>
+    <message>
+        <location filename="../ui/new_peak.ui" line="125"/>
+        <source>Required</source>
+        <translation>Erforderlich</translation>
+    </message>
+    <message>
+        <location filename="../ui/new_peak.ui" line="158"/>
+        <source>Add peak</source>
+        <translation>Gipfel hinzufügen</translation>
+    </message>
+    <message>
+        <location filename="../ui/new_peak.ui" line="177"/>
+        <source>Cancel</source>
+        <translation>Abbrechen</translation>
+    </message>
+</context>
+<context>
+    <name>NewRangeDialog</name>
+    <message>
+        <location filename="../ui/new_range.ui" line="14"/>
+        <source>New mountain range</source>
+        <translation>Neues Gebirge</translation>
+    </message>
+    <message>
+        <location filename="../ui/new_range.ui" line="43"/>
+        <source>Name</source>
+        <translation>Name</translation>
+    </message>
+    <message>
+        <location filename="../ui/new_range.ui" line="50"/>
+        <source>Required</source>
+        <translation>Erforderlich</translation>
+    </message>
+    <message>
+        <location filename="../ui/new_range.ui" line="57"/>
+        <source>Continent</source>
+        <translation>Kontinent</translation>
+    </message>
+    <message>
+        <location filename="../ui/new_range.ui" line="64"/>
+        <source>Select</source>
+        <translation>Auswählen</translation>
+    </message>
+    <message>
+        <location filename="../ui/new_range.ui" line="74"/>
+        <source>Region</source>
+        <translation>Region</translation>
+    </message>
+    <message>
+        <location filename="../ui/new_range.ui" line="107"/>
+        <source>Add mountain range</source>
+        <translation>Gebirge hinzufügen</translation>
+    </message>
+    <message>
+        <location filename="../ui/new_range.ui" line="126"/>
+        <source>Cancel</source>
+        <translation>Abbrechen</translation>
+    </message>
+</context>
+<context>
+    <name>NewTripDialog</name>
+    <message>
+        <location filename="../ui/new_trip.ui" line="20"/>
+        <source>New trip</source>
+        <translation>Neue Reise</translation>
+    </message>
+    <message>
+        <location filename="../ui/new_trip.ui" line="49"/>
+        <source>Name</source>
+        <translation>Name</translation>
+    </message>
+    <message>
+        <location filename="../ui/new_trip.ui" line="56"/>
+        <source>Required</source>
+        <translation>Erforderlich</translation>
+    </message>
+    <message>
+        <location filename="../ui/new_trip.ui" line="63"/>
+        <source>Date</source>
+        <translation>Zeitraum</translation>
+    </message>
+    <message>
+        <location filename="../ui/new_trip.ui" line="72"/>
+        <source>Unspecified</source>
+        <translation>Nicht angeben</translation>
+    </message>
+    <message>
+        <location filename="../ui/new_trip.ui" line="81"/>
+        <source>Start date</source>
+        <translation>Anfangsdatum</translation>
+    </message>
+    <message>
+        <location filename="../ui/new_trip.ui" line="88"/>
+        <source>End date</source>
+        <translation>Enddatum</translation>
+    </message>
+    <message>
+        <location filename="../ui/new_trip.ui" line="169"/>
+        <source>Add trip</source>
+        <translation>Reise hinzufügen</translation>
+    </message>
+    <message>
+        <location filename="../ui/new_trip.ui" line="188"/>
+        <source>Cancel</source>
+        <translation>Abbrechen</translation>
     </message>
 </context>
 </TS>
