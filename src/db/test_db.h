594f22a0198889e93a7c9cc1610e9201140f19a6
Simon Vetter <simetist@gmail.com>
2023-02-05 13:12:07 +0100

    Fixed some problems in test database header

--- src/db/test_db.h
+++ src/db/test_db.h
@@ -5,36 +5,36 @@
 
 
 
-const auto CREATE_ASCENTS		= QLatin1String(R"(CREATE TABLE Ascents(ascentID INTEGER PRIMARY KEY, title NVARCHAR, peakID INTEGER REFERENCES Peaks(peakID), date DATE, perDayIndex INTEGER NOT NULL, time TIME, kind INT NOT NULL, traverse BINARY NOT NULL, difficultySystem INTEGER, difficultyGrade INTEGER, tripID INTEGER REFERENCES Trips(tripID), notes NVARCHAR))");
-const auto CREATE_PEAKS			= QLatin1String(R"(CREATE TABLE Peaks(peakID INTEGER PRIMARY KEY, name NVARCHAR NOT NULL, height INTEGER, volcano BINARY NOT NULL, regionID INTEGER REFERENCES Regions(regionID), mapsLink NVARCHAR, earthLink NVARCHAR, wikiLink NVARCHAR))");
-const auto CREATE_TRIPS			= QLatin1String(R"(CREATE TABLE Trips(tripID INTEGER PRIMARY KEY, name NVARCHAR NOT NULL, startDate DATE, endDate DATE, notes NVARCHAR))");
-const auto CREATE_HIKERS		= QLatin1String(R"(CREATE TABLE Hikers(hikerID INTEGER PRIMARY KEY, name NVARCHAR NOT NULL))");
-const auto CREATE_REGIONS		= QLatin1String(R"(CREATE TABLE Regions(regionID INTEGER PRIMARY KEY, name NVARCHAR NOT NULL, rangeID INTEGER REFERENCES Ranges(rangeID), countryID INTEGER REFERENCES Countries(countryID)))");
-const auto CREATE_RANGES		= QLatin1String(R"(CREATE TABLE Ranges(rangeID INTEGER PRIMARY KEY, name NVARCHAR NOT NULL, continent INT))");
-const auto CREATE_COUNTRIES		= QLatin1String(R"(CREATE TABLE Countries(countryID INTEGER PRIMARY KEY, name NVARCHAR NOT NULL))");
-const auto CREATE_PHOTOS		= QLatin1String(R"(CREATE TABLE Photos(photoID INTEGER PRIMARY KEY, ascentID INTEGER REFERENCES Ascents(ascentID), localIndex INTEGER NOT NULL, filepath NVARCHAR NOT NULL))");
-const auto CREATE_PARTICIPATED	= QLatin1String(R"(CREATE TABLE Participated(ascentID INTEGER NOT NULL, hikerID INTEGER NOT NULL, CONSTRAINT participatedPK PRIMARY KEY (ascentID, hikerID)))");
+const auto CREATE_ASCENTS		= "CREATE TABLE Ascents(ascentID INTEGER PRIMARY KEY, title NVARCHAR, peakID INTEGER REFERENCES Peaks(peakID), date DATE, peakOnDay INTEGER NOT NULL, time TIME, hikeKind INT NOT NULL, traverse BINARY NOT NULL, difficultySystem INTEGER, difficultyGrade INTEGER, tripID INTEGER REFERENCES Trips(tripID), notes NVARCHAR)";
+const auto CREATE_PEAKS			= "CREATE TABLE Peaks(peakID INTEGER PRIMARY KEY, name NVARCHAR NOT NULL, height INTEGER, volcano BINARY NOT NULL, regionID INTEGER REFERENCES Regions(regionID), mapsLink NVARCHAR, earthLink NVARCHAR, wikiLink NVARCHAR)";
+const auto CREATE_TRIPS			= "CREATE TABLE Trips(tripID INTEGER PRIMARY KEY, name NVARCHAR NOT NULL, startDate DATE, endDate DATE, notes NVARCHAR)";
+const auto CREATE_HIKERS		= "CREATE TABLE Hikers(hikerID INTEGER PRIMARY KEY, name NVARCHAR NOT NULL)";
+const auto CREATE_REGIONS		= "CREATE TABLE Regions(regionID INTEGER PRIMARY KEY, name NVARCHAR NOT NULL, rangeID INTEGER REFERENCES Ranges(rangeID), countryID INTEGER REFERENCES Countries(countryID))";
+const auto CREATE_RANGES		= "CREATE TABLE Ranges(rangeID INTEGER PRIMARY KEY, name NVARCHAR NOT NULL, continent INT)";
+const auto CREATE_COUNTRIES		= "CREATE TABLE Countries(countryID INTEGER PRIMARY KEY, name NVARCHAR NOT NULL)";
+const auto CREATE_PHOTOS		= "CREATE TABLE Photos(photoID INTEGER PRIMARY KEY, ascentID INTEGER REFERENCES Ascents(ascentID), localIndex INTEGER NOT NULL, filepath NVARCHAR NOT NULL)";
+const auto CREATE_PARTICIPATED	= "CREATE TABLE Participated(ascentID INTEGER NOT NULL, hikerID INTEGER NOT NULL, CONSTRAINT participatedPK PRIMARY KEY (ascentID, hikerID))";
 
-const auto INSERT_ASCENT		= QLatin1String(R"(INSERT INTO Ascents(title, peakID, date, perDayIndex, time, kind, traverse, difficultySystem, difficultyGrade, tripID, notes) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?))");
-const auto INSERT_PEAK			= QLatin1String(R"(INSERT INTO Peaks(name, height, volcano, regionID, mapsLink, earthLink, wikiLink) values(?, ?, ?, ?, ?, ?, ?))");
-const auto INSERT_TRIP			= QLatin1String(R"(INSERT INTO Trips(name, startDate, endDate, notes) values(?, ?, ?, ?))");
-const auto INSERT_HIKER			= QLatin1String(R"(INSERT INTO Hikers(name) values(?))");
-const auto INSERT_REGION		= QLatin1String(R"(INSERT INTO Regions(name, rangeID, countryID) values(?, ?, ?))");
-const auto INSERT_RANGE			= QLatin1String(R"(INSERT INTO Ranges(name, continent) values(?, ?))");
-const auto INSERT_COUNTRY		= QLatin1String(R"(INSERT INTO Countries(name) values(?))");
-const auto INSERT_PHOTO			= QLatin1String(R"(INSERT INTO Photos(ascentID, index, filepath) values(?, ?, ?))");
-const auto INSERT_PARTICIPATED	= QLatin1String(R"(INSERT INTO Trips(ascentID, hikerID) values(?, ?))");
+const auto INSERT_ASCENT		= "INSERT INTO Ascents(title, peakID, date, peakOnDay, time, hikeKind, traverse, difficultySystem, difficultyGrade, tripID, notes) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+const auto INSERT_PEAK			= "INSERT INTO Peaks(name, height, volcano, regionID, mapsLink, earthLink, wikiLink) values(?, ?, ?, ?, ?, ?, ?)";
+const auto INSERT_TRIP			= "INSERT INTO Trips(name, startDate, endDate, notes) values(?, ?, ?, ?)";
+const auto INSERT_HIKER			= "INSERT INTO Hikers(name) values(?)";
+const auto INSERT_REGION		= "INSERT INTO Regions(name, rangeID, countryID) values(?, ?, ?)";
+const auto INSERT_RANGE			= "INSERT INTO Ranges(name, continent) values(?, ?)";
+const auto INSERT_COUNTRY		= "INSERT INTO Countries(name) values(?)";
+const auto INSERT_PHOTO			= "INSERT INTO Photos(ascentID, index, filepath) values(?, ?, ?)";
+const auto INSERT_PARTICIPATED	= "INSERT INTO Participated(ascentID, hikerID) values(?, ?)";
 
 
 
-int addAscent(QSqlQuery& q, const QString& title, int peakID, const QDate& date, int perDayIndex, const QTime& time, int kind, bool traverse, int difficultySystem, int difficultyGrade, int tripID, const QString& notes)
+int addAscent(QSqlQuery& q, const QString& title, int peakID, const QDate& date, int peakOnDay, const QTime& time, int hikeKind, bool traverse, int difficultySystem, int difficultyGrade, int tripID, const QString& notes)
 {
 	q.addBindValue(title);
 	q.addBindValue(peakID);
 	q.addBindValue(date);
-	q.addBindValue(perDayIndex);
+	q.addBindValue(peakOnDay);
 	q.addBindValue(time);
-	q.addBindValue(kind);
+	q.addBindValue(hikeKind);
 	q.addBindValue(traverse);
 	q.addBindValue(difficultySystem);
 	q.addBindValue(difficultyGrade);
@@ -117,13 +117,13 @@ int addParticipated(QSqlQuery& q, int ascentID, int hikerID)
 
 
 
-QSqlError initDb()
+QSqlError initDB()
 {
-	QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
-	db.setDatabaseName(":memory:");
+	QSqlDatabase sql = QSqlDatabase::addDatabase("QSQLITE");
+	sql.setDatabaseName(":memory:");
 	
-	if (!db.open())
-		return db.lastError();
+	if (!sql.open())
+		qDebug() << sql.lastError();
 	
 	QSqlQuery q;
 	if (!q.exec(CREATE_ASCENTS)
@@ -172,7 +172,7 @@ QSqlError initDb()
 	int bandcampID	= addTrip(q, QString("That one time at band camp"),	QDate(2004, 3, 15),		QDate(2004, 4, 2),	QString("We had a pillow fight"));
 	int alpsAgainID	= addTrip(q, QString("Alps again"),					QDate(2020, 3, 12),		QDate(2020, 8, 26),	QString("Train back cancelled for some reason"));
 	
-	if (!q.prepare(INSERT_TRIP)) return q.lastError();
+	if (!q.prepare(INSERT_ASCENT)) return q.lastError();
 	int ascent1_1ID	= addAscent(q, QString("Peak of Europe"),		montblancID,	QDate(1999, 12, 29),	1,	QTime(15, 35),	0,	false,	1,	4,	alpsTripID,		QString("Some notes"));
 	int ascent1_2ID	= addAscent(q, QString("Lake and stuff"),		pfanderID,		QDate(2000, 1, 2),		1,	QTime(13, 48),	0,	false,	1,	4,	alpsTripID,		QString("Some other notes"));
 	int ascent2_1ID	= addAscent(q, QString("A view to a Spain"),	pyrenMntnID,	QDate(2004, 3, 25),		1,	QTime(15, 35),	2,	false,	1,	4,	bandcampID,		QString("Other notes still"));
@@ -180,7 +180,7 @@ QSqlError initDb()
 	int ascent3_2ID	= addAscent(q, QString("Feels wrong"),			brockenID,		QDate(2020, 3, 16),		2,	QTime(16, 5),	3,	true,	1,	4,	alpsAgainID,	QString("These are not the same notes"));
 	int ascent3_3ID	= addAscent(q, QString("I can see my house"),	zugspitzeID,	QDate(2020, 7, 19),		1,	QTime(14, 55),	0,	false,	1,	4,	alpsAgainID,	QString("These aren't notes"));
 	
-	if (!q.prepare(INSERT_HIKER)) return q.lastError();
+	if (!q.prepare(INSERT_PARTICIPATED)) return q.lastError();
 	addParticipated(q, ascent1_1ID, aliceID);
 	addParticipated(q, ascent1_2ID, aliceID);
 	addParticipated(q, ascent1_2ID, charlieID);
@@ -217,9 +217,11 @@ QSqlError initDb()
 	addPhoto(q, ascent3_3ID, 1, QString("M:/Photos/2020/Alps/2452.jpg"));
 	addPhoto(q, ascent3_3ID, 2, QString("M:/Photos/2020/Alps/2503.jpg"));
 	
+	qDebug() << "Database initialized with sample data";
+	
 	return QSqlError();
 }
 
 
 
-#endif
+#endif // TEST_DB_H
