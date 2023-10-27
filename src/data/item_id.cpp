4fb9d95f8f736788df47ecdceaabf029e7b9ff4f
Simon Vetter <simetist@gmail.com>
2023-10-27 16:54:17 +0200

    Add documentation for ItemID
    For #163

--- src/data/item_id.cpp
+++ src/data/item_id.cpp
@@ -15,6 +15,12 @@
  * If not, see <https://www.gnu.org/licenses/>.
  */
 
+/**
+ * @file ItemID.h
+ * 
+ * This file defines the ItemID class and its subclass ValidItemID.
+ */
+
 #include "item_id.h"
 
 #include <QHashFunctions>
@@ -26,33 +32,72 @@ const int ItemID::LOWEST_LEGAL_ID = 1;
 
 
 
+/**
+ * Creates an ItemID from an integer index.
+ * 
+ * @param id	The index.
+ */
 ItemID::ItemID(int id) : valid(id >= LOWEST_LEGAL_ID), id(id)
 {}
 
+/**
+ * Creates an ItemID from a QVariant.
+ * 
+ * Returns an invalid ItemID if the QVariant cannot be converted to an integer.
+ * 
+ * @param id	The index as a QVariant.
+ */
 ItemID::ItemID(QVariant id) : valid(id.isValid() && id.canConvert<int>() && id.toInt() >= LOWEST_LEGAL_ID), id(id.toInt())
 {}
 
+/**
+ * Creates an invalid ItemID.
+ */
 ItemID::ItemID() : valid(false), id(LOWEST_LEGAL_ID - 1)
 {}
 
 
 
+/**
+ * Check whether this ItemID is valid.
+ * 
+ * @return	True if this ItemID is valid, false otherwise.
+ */
 bool ItemID::isValid() const
 {
 	return valid;
 }
 
+/**
+ * Check whether this ItemID is invalid.
+ * 
+ * @return	True if this ItemID is invalid, false otherwise.
+ */
 bool ItemID::isInvalid() const
 {
 	return !valid;
 }
 
+/**
+ * Get the integer index of this ItemID, assuming it is valid.
+ * 
+ * @pre This ItemID is valid. If it is not, an assertion fails.
+ * 
+ * @return	The integer index.
+ */
 int ItemID::get() const
 {
 	assert(valid);
 	return id;
 }
 
+/**
+ * Get the integer index of this ItemID.
+ * 
+ * If this ItemID is invalid, returns an invalid QVariant.
+ * 
+ * @return	A QVariant containing the integer index, or an invalid QVariant.
+ */
 QVariant ItemID::asQVariant() const
 {
 	if (isValid()) {
@@ -62,6 +107,13 @@ QVariant ItemID::asQVariant() const
 	}
 }
 
+/**
+ * Asserts that this ItemID is valid and returns an equivalent ValidItemID.
+ * 
+ * @pre This ItemID is valid. If it is not, an assertion fails.
+ * 
+ * @return	A ValidItemID with the same valid integer index.
+ */
 ValidItemID ItemID::forceValid() const
 {
 	assert(valid);
@@ -72,11 +124,25 @@ ValidItemID ItemID::forceValid() const
 
 
 
+/**
+ * Creates a ValidItemID from an integer index.
+ * 
+ * @pre The index represents a valid ID. If it does not, an assertion fails.
+ * 
+ * @param id	The index.
+ */
 ValidItemID::ValidItemID(int id) : ItemID(id)
 {
 	assert(isValid());
 }
 
+/**
+ * Creates a ValidItemID from a QVariant.
+ * 
+ * @pre The QVariant represents a valid ID. If it does not, an assertion fails.
+ * 
+ * @param id	The index as a QVariant.
+ */
 ValidItemID::ValidItemID(QVariant id) : ItemID(id)
 {
 	assert(isValid());
--- src/data/item_id.h
+++ src/data/item_id.h
@@ -15,6 +15,12 @@
  * If not, see <https://www.gnu.org/licenses/>.
  */
 
+/**
+ * @file ItemID.h
+ * 
+ * This file declares the ItemID class and its subclass ValidItemID.
+ */
+
 #ifndef ITEM_ID_H
 #define ITEM_ID_H
 
@@ -26,10 +32,20 @@ class ValidItemID;
 
 
 
+/**
+ * A small wrapper class for an integer ID with a validity flag.
+ * 
+ * An invalid ItemID represents the absence of an ID in the database system.
+ * It does **not** represent an ID for an item which no longer exists: The validity flag only
+ * indicates whether the ID index is valid, not whether a reference using this ID is valid.
+ */
 class ItemID {
+	/** Indicates whether the ID is valid. */
 	bool valid;
+	/** The ID index, if valid. */
 	int id;
 	
+	/** The lowest integer that can be used as an ID in the database system. */
 	static const int LOWEST_LEGAL_ID;
 	
 public:
@@ -48,6 +64,12 @@ public:
 
 
 
+/**
+ * A small wrapper class for a valid integer ID.
+ *
+ * Using this class only indicates that the ID index is valid, not whether a reference using this
+ * ID is valid.
+ */
 class ValidItemID : public ItemID {
 public:
 	ValidItemID(int id);
@@ -56,6 +78,16 @@ public:
 
 
 
+/**
+ * Checks whether the given ItemIDs are equal to each other in validity and potentially value.
+ * 
+ * If both ItemIDs are invalid, they are considered equal.
+ Otherwise, they are considered equal if both are valid and have the same value.
+ * 
+ * @param id1	The first ItemID to compare.
+ * @param id2	The second ItemID to compare.
+ * @return		True if the ItemIDs are equal, false otherwise.
+ */
 inline bool operator==(const ItemID& id1, const ItemID& id2)
 {
 	if (id1.isInvalid() && id2.isInvalid()) return true;
@@ -63,11 +95,31 @@ inline bool operator==(const ItemID& id1, const ItemID& id2)
 	return id1.get() == id2.get();
 }
 
+/**
+ * Checks whether the given ItemIDs are not equal to each other in validity or potentially value.
+ * 
+ * If one ItemID is invalid and the other is valid, they are considered not equal.
+ * Otherwise, they are considered not equal if both are valid and have different values.
+ * 
+ * @param id1	The first ItemID to compare.
+ * @param id2	The second ItemID to compare.
+ * @return		True if the ItemIDs are not equal, false otherwise.
+ */
 inline bool operator!=(const ItemID& id1, const ItemID& id2)
 {
 	return !operator==(id1, id2);
 }
 
+/**
+ * Hashes the given ItemID using the given seed.
+ * 
+ * This implementation is required for using ItemID in QSets and implicitly comparing instances of
+ * QList<ItemID>.
+ * 
+ * @param key	The ItemID to hash.
+ * @param seed	The seed to use for hashing.
+ * @return		The hash value.
+ */
 inline size_t qHash(const ItemID& key, size_t seed)
 {
 	return qHashMulti(seed, key.isValid(), key.isValid() ? key.get() : 0);
